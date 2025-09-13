95ee8c9a9142c449afddc5ef0528fc3fe924ca8e
Kseniya <baharevaxen@yandex.ru>
2025-09-13 17:37:32 +0300

    correction options 2

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from src. options import Connect
 
 if __name__ =="__main__":
     api = Connect()
-    vacancies = api.vacancies("разработчик")  # Передаем ключевое слово
+    vacancies = api.vacancies("разработчик")
     for vacancy in vacancies:
         print(vacancy['name'], vacancy['url'])
